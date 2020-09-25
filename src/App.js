555db35bc09f13ca9d5adc003c23bfc3f356d53e
Tom Scolaro <38729245+tomscolaro@users.noreply.github.com>
2020-09-25 08:19:23 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,27 +3,7 @@ import "./App.css";
 import { select } from "d3";
 
 function App() {
-
-
   const [data, setData] = useState([25, 30, 45, 60, 20]);
-
-  if (saved_data.length > 0) {
-    callback(null, parse(variable, saved_data)); 
-  }
-  else {
-  // -------------- Begin Request New Data ------------------
-    d3.json(" ... ", 
-        function(data) {
-          if (!data) return callback(new Error("unable to load data"));   
-          saved_data = data; 
-          // Put it here.
-          callback(null, parse(variable, saved_data));
-        });
-  } 
-
-
-
-
   const svgRef = useRef();
   useEffect(() => {
     const svg = select(svgRef.current);
@@ -36,13 +16,6 @@ function App() {
       .attr("cy", value => value * 2)
       .attr("stroke", "red");
   }, [data]);
-
-
-
-
-
-
-
   return (
     <React.Fragment>
       <svg ref={svgRef}></svg>
