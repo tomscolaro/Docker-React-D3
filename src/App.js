ad5c34cddfbaaba440f221deaf0ef27eda79c251
Tom Scolaro <38729245+tomscolaro@users.noreply.github.com>
2020-09-24 22:56:48 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import React, {useRef, useEffect} from 'react';
-import logo from './logo.svg';
-import * as d3 from "d3";
-import './App.css';
-
-const data = [25,35,45,69,12]
+import React, { useRef, useEffect, useState } from "react";
+import "./App.css";
+import { select } from "d3";
 
 function App() {
-  const svgRef = useRef()
 
+
+  const [data, setData] = useState([25, 30, 45, 60, 20]);
+
+  if (saved_data.length > 0) {
+    callback(null, parse(variable, saved_data)); 
+  }
+  else {
+  // -------------- Begin Request New Data ------------------
+    d3.json(" ... ", 
+        function(data) {
+          if (!data) return callback(new Error("unable to load data"));   
+          saved_data = data; 
+          // Put it here.
+          callback(null, parse(variable, saved_data));
+        });
+  } 
+
+
+
+
+  const svgRef = useRef();
   useEffect(() => {
-    const svg = d3.select(svgRef.current);
-    svg.selectAll("circle").data(data)
-  }, [])
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      .attr("r", value => value)
+      .attr("cx", value => value * 2)
+      .attr("cy", value => value * 2)
+      .attr("stroke", "red");
+  }, [data]);
+
+
+
+
+
+
 
   return (
     <React.Fragment>
       <svg ref={svgRef}></svg>
+      <br />
+      <button onClick={() => setData(data.map(value => value + 5))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data.filter(value => value < 35))}>
+        Filter data
+      </button>
     </React.Fragment>
-
   );
 }
 
