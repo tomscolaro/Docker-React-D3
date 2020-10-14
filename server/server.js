b129c2df2466dcdfe2eeb1aef8f79926607d999f
Tom Scolaro <38729245+tomscolaro@users.noreply.github.com>
2020-10-13 22:23:49 -0500

    Add Dockerfile and server.js

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+
+    if (req.method === "GET") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        fs.createReadStream("./public/form.html", "UTF-8").pipe(res);
+    } else if (req.method === "POST") {
+    
+        var body = "";
+        req.on("data", function (chunk) {
+            body += chunk;
+        });
+
+        req.on("end", function(){
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(body);
+        });
+    }
+
+}).listen(3000);
